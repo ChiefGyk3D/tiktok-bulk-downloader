c27b368503553f96c96dd293c454d9e6d1bd5b0e
ChiefGyk3D <chiefgyk3d@pop-os.localdomain>
2023-03-31 10:27:26 -0400

    fixing logic

--- tiktok_bulk_downloader.py
+++ tiktok_bulk_downloader.py
@@ -1,6 +1,6 @@
 import json
 import os
-from tiktok_scraper import TikTokScraper
+import subprocess
 
 # Load configuration from config.json
 with open('config.json', 'r') as file:
@@ -11,9 +11,6 @@ download_folder = config["download_folder"]
 
 # Ensure the download folder exists
 os.makedirs(download_folder, exist_ok=True)
-
-# Initialize the TikTokScraper
-scraper = TikTokScraper()
 
 def is_downloaded(video_id):
     """Check if a video has been downloaded already."""
@@ -26,26 +23,23 @@ def download_videos(videos):
     """Download TikTok videos without a watermark."""
     for video in videos:
         video_id = video['id']
-        video_url = video['video']['playAddr']
 
         # Check if the video has already been downloaded
         if not is_downloaded(video_id):
             # Download the video without the watermark
-            scraper.download_video(video_url, os.path.join(download_folder, f'{video_id}.mp4'), wm=False)
+            command = f"tiktok-scraper video {video['video']['playAddr']} -o {download_folder} -w"
+            subprocess.run(command, shell=True, check=True)
             print(f"Downloaded video {video_id}")
         else:
             print(f"Skipped video {video_id} (already downloaded)")
 
 def main():
     # Get the user's videos
-    user_videos = scraper.user_videos(username)
+    command = f"tiktok-scraper user {username} -t json -n 0"
+    user_videos_raw = subprocess.check_output(command, shell=True)
+user_videos = json.loads(user_videos_raw)["collector"]
 
-    # Download the videos
-    download_videos(user_videos)
-
-if __name__ == "__main__":
-    main()
-# Get the user's videos
-user_videos = scraper.user_videos(username)
 # Download the videos
 download_videos(user_videos)
+if name == "main":
+    main()
